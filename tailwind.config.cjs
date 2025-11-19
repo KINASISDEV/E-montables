8ca8118eca3268b1a14a66b336692c17dabc2469
Ivan Gonzalez <108gonzalezdelgado@gmail.com>
2025-11-18 19:11:32 -0600

    fix(button):Implementación del boton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,6 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        'header-bg': '#252F3E',
-        'primary': '#FF2800',   
-        'secondary': '#DCC24C', 
-      },
       fontFamily: {
         'brand': ['Audiowide', 'cursive'],
         'body': ['Poppins', 'sans-serif'],
